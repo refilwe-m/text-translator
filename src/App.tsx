265aef442838c44c9329570a680d9350ffee2a22
Refilwe Mashile <refilwe@codehesion.co.za>
2023-08-04 00:13:44 +0200

    UI Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Heading } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [language, setLanguage] = useState("english");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Heading text="Text Translator" />
+      <section className="input-section">
+        <input
+          className="px-4 py-2 border border-gray-400 rounded-lg"
+          type="text"
+          placeholder={language}
+          onChange={(e) => setLanguage(e.target.value)}
+        />
+        <button
+          className="bg-teal-400"
+          onClick={() => alert(`Your language is ${language}`)}
+        >
+          Translate
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      </section>
+
+      <p>
+        I love peace and solitude.
+        <br /> I learn better in quietness and I prefer to watch the sky and
+        <br /> sing songs in my heart than to be in a loud city
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/heading/heading.tsx
@@ -0,0 +1,3 @@
+export const Heading = ({ text }: { text: string }) => {
+  return <h1 className="text-6xl">{text}</h1>;
+};
